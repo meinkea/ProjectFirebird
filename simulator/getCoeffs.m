7fd16975c4e1b90bc7131c6bfc44fcf4a689ab52
DJSylveon <57302026+DJSylveon@users.noreply.github.com>
2020-01-21 18:05:33 -0500

    Changed to work with elevator settings

--- simulator/getCoeffs.m
+++ simulator/getCoeffs.m
@@ -1,5 +1,5 @@
 %% Function to determine aerodynamic coefficients
-function [CL,CD,CM] = getCoeffs(alpha,a,cl,cd,cm)
+function [CL,CD,CM] = getCoeffs(alpha,a,cl,cd,cm,elvDef)
 % INPUTS:
 %   a, cl, cd, & cm are all arrays based on 0 elevator deflection
 % OUTPUTS:
@@ -15,7 +15,15 @@ function [CL,CD,CM] = getCoeffs(alpha,a,cl,cd,cm)
 % 'extrap' lets the function extrapolate information if your input is
 %   outside of the given data range
 
-CL = interp1(a,cl,alpha,'pchip','extrap');
-CD = interp1(a,cd,alpha,'pchip','extrap');
-CM = interp1(a,cm,alpha,'pchip','extrap');
+% Define elevator deflections
+def = [-15,0,15]';
+
+CL1 = interp1(a,cl,alpha,'pchip','extrap')';
+CD1 = interp1(a,cd,alpha,'pchip','extrap')';
+CM1 = interp1(a,cm,alpha,'pchip','extrap')';
+
+
+CL = interp1(def,CL1,elvDef,'pchip','extrap');
+CD = interp1(def,CD1,elvDef,'pchip','extrap');
+CM = interp1(def,CM1,elvDef,'pchip','extrap');
 end
