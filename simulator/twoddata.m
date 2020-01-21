f8504a14ebcdd182b0127d5193efbcd84b03fc91
DJSylveon <57302026+DJSylveon@users.noreply.github.com>
2020-01-21 18:07:54 -0500

    Velocity fixed. Degrees used over radians.

--- simulator/twoddata.m
+++ simulator/twoddata.m
@@ -1,16 +1,13 @@
-function [velocity,gamma,height,downrange] = twoddata(thrust,alpha,gamma,velocity,drag,lift,mass,gravity,dtime,height,downrange)
+function [velocity1,gamma1,height1,downrange1] = twoddata(thrust,alpha,gamma,velocity,drag,lift,mass,gravity,dtime,height,downrange)
 %Summary of this function: Does Euler's Method to integrate
 %   Detailed explanation goes here
-veldot = (thrust*cos(alpha)-drag-mass*gravity*sin(gamma))/mass;%acceleration of glider
-gamdot = (thrust*sin(alpha)+lift-mass*gravity*cos(gamma))/(mass*velocity); %pitch rate of glider
-downdot = velocity*cos(gamma); %horizontal velocity of glider
-heightdot = velocity*sin(gamma); %vertical velocity of glider
+veldot = ((thrust*cosd(alpha)-drag)/mass)-(gravity*sind(gamma)); %acceleration of glider
+gamdot = ((thrust*sind(alpha)+lift)/(velocity*mass))-((gravity/velocity)*cosd(gamma)); %pitch rate of glider
+downdot = velocity*cosd(gamma); %horizontal velocity of glider
+heightdot = velocity*sind(gamma); %vertical velocity of glider
 %time step and output
-velocity = velocity + veldot*dtime;
-gamma = gamma + gamdot*dtime;
-height = height + heightdot*dtime;
-downrange = downrange + downdot*dtime;
-
+velocity1 = abs(velocity + veldot*dtime);
+gamma1 = gamma + gamdot*dtime;
+height1 = height + heightdot*dtime;
+downrange1 = downrange + downdot*dtime;
 end
-
-
