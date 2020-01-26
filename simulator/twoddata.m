cbd25d445bbf9c91f3378e07accf694f130a4c1b
DJSylveon <57302026+DJSylveon@users.noreply.github.com>
2020-01-26 15:20:05 -0500

    Added Boost section. Fixed gamma integration.

--- simulator/twoddata.m
+++ simulator/twoddata.m
@@ -1,13 +1,25 @@
-function [velocity1,gamma1,height1,downrange1] = twoddata(thrust,alpha,gamma,velocity,drag,lift,mass,gravity,dtime,height,downrange)
+function [velocity1,gamma1,height1,downrange1] = twoddata(thrust,alpha,gamma,velocity,drag,lift,mass,gravity,dtime,height,downrange,Glide)
 %Summary of this function: Does Euler's Method to integrate
-%   Detailed explanation goes here
+%   The first if statement is for when the glide is in flight. Its assumed
+%   that there is no lift being generated simply because its been trimmed
+%   out to do so. 
+%The second portion is for when the glider is in gliding flight.
+
+if Glide == false
 veldot = ((thrust*cosd(alpha)-drag)/mass)-(gravity*sind(gamma)); %acceleration of glider
-gamdot = ((thrust*sind(alpha)+lift)/(velocity*mass))-((gravity/velocity)*cosd(gamma)); %pitch rate of glider
+gamdot = -((gravity/velocity)*cosd(gamma)); %pitch rate of glider
+downdot = velocity*cosd(gamma); %horizontal velocity of glider
+heightdot = velocity*sind(gamma); %vertical velocity of glider    
+end
+
+veldot = ((thrust*cosd(alpha)-drag)/mass)-(gravity*sind(gamma)); %acceleration of glider
+gamdot = ((thrust*sind(alpha)+lift)/(mass*velocity))-((gravity/velocity)*cosd(gamma)); %pitch rate of glider
 downdot = velocity*cosd(gamma); %horizontal velocity of glider
 heightdot = velocity*sind(gamma); %vertical velocity of glider
+
 %time step and output
 velocity1 = abs(velocity + veldot*dtime);
-gamma1 = gamma + gamdot*dtime;
+gamma1 = gamma + radtodeg(gamdot)*dtime; %radtodeg is needed or else code breaks
 height1 = height + heightdot*dtime;
 downrange1 = downrange + downdot*dtime;
 end
